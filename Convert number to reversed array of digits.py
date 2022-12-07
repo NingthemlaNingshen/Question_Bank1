7cc5ce0b4b7c9c0f7c639bc94bb4a0b97fdd6bd6
NingthemlaNingshen <95535321+NingthemlaNingshen@users.noreply.github.com>
2022-12-07 07:47:28 +0530

    Update Convert number to reversed array of digits.py

--- Convert number to reversed array of digits.py
+++ Convert number to reversed array of digits.py
@@ -1,9 +1,9 @@
 def Convert(n):
     s=str(n)
     l=[]
-    i=0
-    while i<len(s):
-        l.append(int(s[i]))
+    i=1
+    while i<=len(s):
+        l.append(int(s[-i]))
         i=i+1
     return l
-print(Convert(234567))
+print(Convert(234567))  ##to convert into reversed list
